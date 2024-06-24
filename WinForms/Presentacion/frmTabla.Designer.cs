21b53f16226da83f0b6b0599e95ccabd231ed4a2
stcrodrii <cabralrodrigo793@gmail.com>
2024-06-24 16:23:18 -0300

    Añadida nueva carpeta con código y formularios C#

---
+++ WinForms/Presentacion/frmTabla.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Forms.Presentacion
+{
+    partial class frmTabla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtProductoId = new System.Windows.Forms.TextBox();
+            this.txtNombreProducto = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ProductoId";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NombreProducto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cantidad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 179);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Categoria";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(384, 223);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtProductoId
+            // 
+            this.txtProductoId.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtProductoId.ForeColor = System.Drawing.Color.Black;
+            this.txtProductoId.Location = new System.Drawing.Point(356, 29);
+            this.txtProductoId.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtProductoId.Name = "txtProductoId";
+            this.txtProductoId.Size = new System.Drawing.Size(116, 20);
+            this.txtProductoId.TabIndex = 5;
+            // 
+            // txtNombreProducto
+            // 
+            this.txtNombreProducto.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtNombreProducto.Location = new System.Drawing.Point(356, 71);
+            this.txtNombreProducto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtNombreProducto.Name = "txtNombreProducto";
+            this.txtNombreProducto.Size = new System.Drawing.Size(116, 20);
+            this.txtNombreProducto.TabIndex = 6;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCantidad.Location = new System.Drawing.Point(356, 125);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(116, 20);
+            this.txtCantidad.TabIndex = 7;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCategoria.Location = new System.Drawing.Point(356, 174);
+            this.txtCategoria.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(116, 20);
+            this.txtCategoria.TabIndex = 8;
+            // 
+            // frmTabla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(478, 258);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtNombreProducto);
+            this.Controls.Add(this.txtProductoId);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmTabla";
+            this.Text = "DatosTablas";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.frmTabla_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtProductoId;
+        private System.Windows.Forms.TextBox txtNombreProducto;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtCategoria;
+    }
+}
---
+++ WinForms/Presentacion/frmTabla.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Forms.model;
+
+namespace Forms.Presentacion
+{
+    public partial class frmTabla : Form
+    {
+        public int? id ;
+        Productos oTabla = null ; // ----------------------------------------------------------------------------
+        public frmTabla(int? id = null)
+        {
+            InitializeComponent();
+            this.id = id;
+            if (id != null)
+                CargaDatos();
+
+            
+        }
+        private void CargaDatos()
+        {
+            using (PruebaEntities db = new PruebaEntities())
+            {
+                /* oTabla =  db.Productos.Find(id);
+                txtProductoId.Text = int.Parse(oTabla.productoId);
+                txtNombreProducto.Text = oTabla.nombreProducto;
+                txtCantidad.Text = oTabla.cantidad;
+                txtCategoria.Text = oTabla.categoria;*/
+
+                int productoId, cantidad, categoria;
+
+                if (int.TryParse(txtProductoId.Text, out productoId) &&
+                    int.TryParse(txtCantidad.Text, out cantidad) &&
+                    int.TryParse(txtCategoria.Text, out categoria))
+                {
+                    // Conversiones exitosas
+                }
+                else
+                {
+                    // Manejo de error: mostrar un mensaje o tomar alguna acción
+                    MessageBox.Show("Por favor, ingrese valores numéricos válidos.");
+                    return;
+                }
+
+
+            }
+        }
+        /* private void button1_Click(object sender, EventArgs e)
+        {
+            using (PruebaEntities db = new PruebaEntities() )
+            {
+                if(id== null)
+                    oTabla = new Productos();
+
+                Productos productos = new Productos();// esta es la clase q me creo sola entity y se llama como mi tabla Productos
+                productos.productoId = int.Parse(txtProductoId.Text);
+                productos.nombreProducto = txtNombreProducto.Text;
+                productos.cantidad = int.Parse(txtCantidad.Text);
+                productos.categoria = int.Parse(txtCategoria.Text);
+
+
+                if(id==null)
+                    db.Productos.Add(oTabla);
+                else
+                {
+                    db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                }
+
+              //  db.SaveChanges();
+                db.Productos.Add(productos);// agreagr a la base de dato lo q se guardo
+                db.SaveChanges();//esto guarda en la base de datos
+
+                this.Close(); // esto es cunado se guarda todo CIERRRA EL FORMULARIO
+            }
+        }*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (PruebaEntities db = new PruebaEntities())
+            {
+                // Verifica si el ID es nulo o no antes de proceder
+                if (id != null)
+                {
+                    // Buscar la entidad existente
+                    oTabla = db.Productos.Find(id);
+
+                    // Si la entidad existe, actualiza los valores
+                    if (oTabla != null)
+                    {
+                        oTabla.productoId = int.Parse(txtProductoId.Text);
+                        oTabla.nombreProducto = txtNombreProducto.Text;
+                        oTabla.cantidad = int.Parse(txtCantidad.Text);
+                        oTabla.categoria = int.Parse(txtCategoria.Text);
+
+                        // Marca la entidad como modificada
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El producto con el ID especificado no fue encontrado.");
+                        return;
+                    }
+                }
+                else
+                {
+                    // Crear una nueva entidad si el ID es nulo
+                    Productos productos = new Productos
+                    {
+   // ------------------------------ productoId = int.Parse(txtProductoId.Text),
+                        nombreProducto = txtNombreProducto.Text,
+                        cantidad = int.Parse(txtCantidad.Text),
+                        categoria = int.Parse(txtCategoria.Text)
+                    };
+
+                    // Agregar la nueva entidad a la base de datos
+                    db.Productos.Add(productos);
+                }
+
+                // Guardar los cambios en la base de datos
+                db.SaveChanges();
+                MessageBox.Show("Cambios guardados exitosamente.");
+                this.Close(); // Cierra el formulario después de guardar
+            }
+        }
+
+
+        private void frmTabla_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
